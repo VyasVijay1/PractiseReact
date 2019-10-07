f34a991f05e2b7d28c1824d008f2193078af7e5c
VyasVijay1 <learning.curve.42@gmail.com>
2019-10-06 22:58:09 -0700

    commit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import Menu from '../Menu'
-// import PropsBasicContactCard from './ContactCard/PropsContactCard'
-// import MapJoke from '../component/Joke/MapJoke'
-// import MapProduct from '../component/Product/MapProduct'
-// import Todo from './ToDoList/Todo'
-// import BasicClass from './Class/BasicClass'
-// import IsloggedIn1 from './Class/IsloggedIn'
-// import SetState from './Class/ClickEvents/SetState'
-// import PassEvents from './Class/ClickEvents/PassEvent/PassEvents'
-// import LifeCycleEvents from './Class/LifeCycleEvents/LifeCycleEvents'
-// import IsloggedIn from './Class/LoginOut/IsLogged'
-// import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 
 
 class Header extends React.Component {
